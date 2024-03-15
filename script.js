68726961f2e21971cbe4c950820bae63e176df7a
OGMui <80379201+OGMui@users.noreply.github.com>
2024-03-15 16:55:11 +0700

    Add const to script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+const screenText = document.getElementById("screen-text");
+const btnAllClear = document.getElementById('all-clear');
+const btnPosNeg = document.getElementById("pos-neg");
+const btnPercent = document.getElementById("percent");
+const btnDivide = document.getElementById("divide");
+const btnSeven = document.getElementById("seven");
+const btnEight = document.getElementById("eight");
+const btnNine = document.getElementById("nine");
+const btnMultiply = document.getElementById("multiply");
+const btnFour = document.getElementById("four");
+const btnFive = document.getElementById("five");
+const btnSix = document.getElementById("six");
+const btnMinus = document.getElementById("minus");
+const btnOne = document.getElementById("one");
+const btnTwo = document.getElementById("two");
+const btnThree = document.getElementById("three");
+const btnPlus = document.getElementById("plus");
+const btnZero = document.getElementById("zero");
+const btnDecimal = document.getElementById("decimal");
+const btnEquals = document.getElementById("equals");
+
+
